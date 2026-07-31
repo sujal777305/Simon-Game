3af195f3c1c9a6c88bbd1c2ac85f71f42c871b53
sujal777305 <sujalpatel01234@gmail.com>
2026-07-31 11:29:53 +0530

    Step 2 Complete

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 let gameSq=[];
 let userSq=[];
 
+let btns = ["yellow", "red", "purple", "green"];
+
 let started=false;
 let level=0;
+
+let h2=document.querySelector('h2');
 
 document.addEventListener("keypress", function() {
     if(started==false) {
         started=true;
+        levelUp();
     }
 });
+
+function btnFlash(btn) {
+    btn.classList.add("flash");
+
+    setTimeout(function() {
+        btn.classList.remove("flash");
+    }, 250);
+}
+
+function levelUp() {
+    level++;
+    h2.innerText=`Level ${level}`;
+
+    let randIdx = Math.floor(Math.random()*4);
+    let randColor=btns[randIdx];
+    let randBtn=document.querySelector(`.${randColor}`);
+    btnFlash(randBtn);
+}
